5ac3b6ebc420d9165abef0c885cb6b33a15b8351
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-04-05 12:55:57 +0200

    He avanzado con el home, el proximo dia lo termino le falta el footer y algunos elementos

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 
 @NgModule({
   declarations: [
+  
+    FooterComponent
   ],
   imports: [
     CommonModule
