8d6de448f86fea6c16c702c0ce26cee9ec79420e
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-05-15 18:07:38 +0200

    Prueba notyf

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
+import 'notyf/notyf.min.css';
+
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
   ngZoneEventCoalescing: true,
