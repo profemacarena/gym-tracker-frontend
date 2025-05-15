aa71f40dbb4d8b9fd690866b5abbecb1b73e0107
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-05-15 18:20:20 +0200

    Prueba notfy

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
-import 'notyf/notyf.min.css';
 
 
 platformBrowserDynamic().bootstrapModule(AppModule, {
