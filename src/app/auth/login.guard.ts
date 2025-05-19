a223e3992f58c4faf5a2f7d1703d7e45262dfc61
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-05-19 17:22:26 +0200

    Arreglado el bug de los guards

--- src/app/auth/login.guard.ts
+++ src/app/auth/login.guard.ts
@@ -1,25 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, CanMatch, GuardResult, MaybeAsync, Route, Router, RouterStateSnapshot, UrlSegment } from '@angular/router';
-import { Observable, tap } from 'rxjs';
+import { map, Observable, tap } from 'rxjs';
 import { AuthService } from './auth.service';
 
 @Injectable({providedIn: 'root'})
-export class LoginGuard implements CanMatch,CanActivate{
-    constructor(private authService:AuthService, private router:Router) { }
-    canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
-        return this.checkAuthStatus();    
-      }
-     
-      canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {   
-        return this.checkAuthStatus();
-      }
-      private checkAuthStatus(): boolean | Observable<boolean>{
-        return this.authService.checkAuthenticacion()
-          .pipe(
-            tap( isAuthenticated => {
-              if ( isAuthenticated ){
-                this.router.navigate(['/workout/dashboard'])
-              }})
-          )
-      } 
+export class LoginGuard implements CanMatch, CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
+
+  canMatch(route: Route, segments: UrlSegment[]): MaybeAsync<GuardResult> {
+    return this.checkAuthStatus();    
+  }
+  
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): MaybeAsync<GuardResult> {   
+    return this.checkAuthStatus();
+  }
+
+  private checkAuthStatus(): boolean | Observable<boolean> {
+    return this.authService.checkAuthenticacion().pipe(
+      tap(isAuthenticated => {
+        if (isAuthenticated) {
+          this.router.navigate(['/workout/dashboard']);
+        }
+      }),
+      tap(),
+      map(isAuthenticated => !isAuthenticated)
+    );
+  }
 }
+
