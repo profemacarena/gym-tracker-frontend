a41fdd02f1b5ca1c95f1a7bcaf3191f5bae69830
Adrian Contreras <contrerasbuenoadrian@gmail.com>
2025-05-19 17:11:05 +0200

    Guards completos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { AuthGuard } from './auth/auth.guard';
+import { LoginGuard } from './auth/login.guard';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
-  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule),
+    canActivate:[LoginGuard],
+    canMatch:[LoginGuard]
+   },
   { path: 'workout', loadChildren: () => import('./workout/workout.module').then(m => m.WorkoutModule),
     canActivate:[AuthGuard],
     canMatch:[AuthGuard],
